b44c982ef85911a32c2241fc37bf821bba34b7f9
jyr <jyr@wavers.co.kr>
2022-09-24 12:57:07 +0900

    when 표현식, 문자열 템플릿

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -6,26 +6,37 @@ fun main(args: Array<String>) {
 
     // 아우라
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-
     val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println(auraColor)
 
-    val healthStatus = if (healthPoints == 100) {
-        "최상의 상태임!"
-    } else if (healthPoints in 90..99) {
-        "약간의 찰과상만 있음."
-    } else if (healthPoints in 75..89) {
-        if (isBlessed) {
-            "경미한 상처가 있지만 빨리 치유됨!"
-        } else {
-            "경미한 상처만 있음."
-        }
-    } else if (healthPoints in 15..74) {
-        "많이 다친 것 같음."
-    } else {
-        "최악의 상태임!"
+//    val healthStatus = if (healthPoints == 100) {
+//        "최상의 상태임!"
+//    } else if (healthPoints in 90..99) {
+//        "약간의 찰과상만 있음."
+//    } else if (healthPoints in 75..89) {
+//        if (isBlessed) {
+//            "경미한 상처가 있지만 빨리 치유됨!"
+//        } else {
+//            "경미한 상처만 있음."
+//        }
+//    } else if (healthPoints in 15..74) {
+//        "많이 다친 것 같음."
+//    } else {
+//        "최악의 상태임!"
+//    }
+
+    val healthStatus = when (healthPoints) {
+        100 -> "최상의 상태임!"
+        in 90..99 -> "약간의 찰과상만 있음."
+        in 75..89 -> if (isBlessed) {
+                "경미한 상처가 있지만 빨리 치유됨!"
+            } else {
+                "경미한 상처만 있음."
+            }
+        in 15..74 -> "많이 다친 것 같음."
+        else -> "최악의 상태임!"
     }
 
     // 플레이어의 상태 출력
-    println(name + " " + healthStatus)
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
 }
