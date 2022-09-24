7bb6725a4c18c3e34c41e030b3fa108b24d9b9f5
jyr <jyr@wavers.co.kr>
2022-09-24 12:35:03 +0900

    if/else 문, 중첩 if/else 문, 논리 연산자, 조건 표현식, if/else 표현식에서 괄호 제거하기, 범위

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+    val name = "마드리길"
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    // 아우라
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
+
+    val healthStatus = if (healthPoints == 100) {
+        "최상의 상태임!"
+    } else if (healthPoints in 90..99) {
+        "약간의 찰과상만 있음."
+    } else if (healthPoints in 75..89) {
+        if (isBlessed) {
+            "경미한 상처가 있지만 빨리 치유됨!"
+        } else {
+            "경미한 상처만 있음."
+        }
+    } else if (healthPoints in 15..74) {
+        "많이 다친 것 같음."
+    } else {
+        "최악의 상태임!"
+    }
+
+    // 플레이어의 상태 출력
+    println(name + " " + healthStatus)
+}
