6abe18d725b014ae6a13912dbeedc0bc4bda4f17
jyr <jyr@wavers.co.kr>
2022-09-24 18:19:35 +0900

    숫자 타입, 정수, 소수, 문자열을 숫자 타입으로 변환하기, Int 타입을 Double 타입으로 변환하기, Double 타입 값의 형식 지정하기, Double 타입 값을 Int 타입 값으로 변환하기,

--- src/main/kotlin/Tarvern.kt
+++ src/main/kotlin/Tarvern.kt
@@ -1,4 +1,10 @@
+import kotlin.math.roundToInt
+
 const val TAVERN_NAME = "Taernyl's Folly"
+
+var playerGold = 10
+var playerSilver = 10
+
 fun main(args: Array<String>) {
 //    var signatureDrink = "맥주"
 //    signatureDrink = null
@@ -34,7 +40,26 @@ fun main(args: Array<String>) {
 //    println(beverage)
 //    println(beverageServed)
     placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("elixir,Shirly's Temple,5.91")
+//    placeOrder("elixir,Shirly's Temple,5.91")
+}
+
+fun performPurchase(price: Double) {
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver / 100.0)
+    println("지갑 전체 금액: 금화 $totalPurse")
+    println("금화 $price 로 술을 구입함")
+    val remainingBalance = totalPurse - price
+    println("남은 잔액: ${"%.2f".format(remainingBalance)}")
+
+    val remainingGold = remainingBalance.toInt()
+    val remainingSeilver = (remainingBalance % 1 * 100).roundToInt()
+    playerGold = remainingGold
+    playerSilver = remainingSeilver
+    displayBalance()
+}
+
+private fun displayBalance() {
+    println("플레이어의 지갑 잔액: 금화: $playerGold 개, 은화: $playerSilver 개")
 }
 
 private fun toDragonSpeak(phrase: String) = phrase.replace(Regex("[aeiou]")) {
@@ -61,6 +86,8 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
+
+    performPurchase(price.toDouble())
 
 //    val phrase = "와, $name 진짜 좋구나"
 //    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
